223fc3d5b78e5ef62ec4d711254e332ccecccbf5
JHove <jordan.n.hove@ndsu.edu>
2023-03-08 18:37:44 -0600

    Added Jstat, inputs, some outputs done

--- src/nav.js
+++ src/nav.js
@@ -1,19 +1,18 @@
 import { Button, Toolbar, AppBar, Box} from '@mui/material'
-import { color } from '@mui/system';
 import React from 'react'
 
 export default function nav() {
 
-    const pages = ['Continuous Pre Test', 'Continuous Post Test', 'Binomial Pre Test', 'Binomial Post Test'];
+  const pages = ['Continuous Pre Test', 'Continuous Post Test', 'Binomial Pre Test', 'Binomial Post Test'];
 
   return (
     <AppBar position="fixed">
-        <Toolbar sx={{backgroundColor: 'White'}}>
+        <Toolbar sx={{backgroundColor: '#FA4616'}}>
         <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }  }}>
             {pages.map((page) => (
               <Button
                 key={page}
-                sx={{ my: 2, color: '#FA4616', 
+                sx={{ my: 2, color: 'white', 
                 display: 'block', 
                 fontFamily: "sans-serif",
                 fontWeight: 700,
