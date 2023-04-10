0818299924d920c789ceafd8416442c3acbaaf3d
Blake Engrav <blake.engrav@gmail.com>
2023-04-10 18:56:38 -0500

    added educational components

--- src/binomial-posttest-calculator/binomial-posttest-calc.js
+++ src/binomial-posttest-calculator/binomial-posttest-calc.js
@@ -1,4 +1,4 @@
-import { TextField, InputAdornment, FormControl, Box, Tooltip, Button } from "@mui/material";
+import { TextField, InputAdornment, FormControl, Box, Tooltip, Button, TableRow, Table, TableHead, TableCell, TableContainer, Paper } from "@mui/material";
 import "./binomial-posttest-calc.css"
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
@@ -7,6 +7,9 @@ import Typography from '@mui/material/Typography';
 import React, { useState } from "react";
 import Slide from '@mui/material/Slide';
 import norminv from 'norminv';
+import testStatImg from './testStat.png'
+import confIntervalImg from './confInterval.png'
+import paper from '@mui/material/Paper'
 export default function BinomialPostTestCalculator() {
     const [isDetailed, setDetail] = useState(true);
     const [formData, setData] = useState({
@@ -63,7 +66,7 @@ export default function BinomialPostTestCalculator() {
             const value = input.target.value;
             console.log(daysNeeded);
             setData((previous) => {
-                return { ...previous, [name]: value }
+                return { ...previous, [name]: parseInt(value, 10) }
             })
         }
     }
@@ -242,9 +245,9 @@ export default function BinomialPostTestCalculator() {
                     <div hidden={!rejectHypothesis} style={{ color: "black", backgroundColor: "#6eb05d" }}>Reject null hypothesis</div>
                     <Accordion>
                         <AccordionSummary
-                        expandIcon={"▼"}
-                        aria-controls="panel3a-content"
-                        id="panel3a-header"
+                            expandIcon={"▼"}
+                            aria-controls="panel3a-content"
+                            id="panel3a-header"
                         ><Typography>Additional Interpretations</Typography>
                         </AccordionSummary>
                         <AccordionDetails>
@@ -269,6 +272,151 @@ export default function BinomialPostTestCalculator() {
                 </Box>
 
             </div >
+            <Box>
+                <div className="header">Binomial Post-test Education</div>
+                <div>Test Statistic: </div>
+                <Tooltip title={
+                    <React.Fragment>
+                        {"Note: To keep the CI and test conclusion consistent, we used p̂"}<sub>v</sub>{" and p̂"}<sub>c</sub>{" to calculate the test statistic (not pooled conversion rate, p̂)"}
+                    </React.Fragment>
+                }>
+                    <img src={testStatImg} width="30%" height="auto" alt="test statistic equation" />
+                </Tooltip>
+            </Box>
+            <Box>
+                <div>Confidence Interval: </div>
+                <Tooltip title={
+                    <React.Fragment>
+                        {"Where, "}<i>Z</i><sub>1-α/2</sub> {"is a constant. For ex.,for a 95% CI "}<i>Z</i><sub>0.975</sub>{" = 1.96"}
+                    </React.Fragment>
+                }>
+                    <img src={confIntervalImg} width="30%" height="auto" alt="test statistic equation" />
+                </Tooltip>
+            </Box>
+            <Box>
+                <TableContainer sx={{ height: "auto" }} component={Paper}>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Parameter Estimates</TableCell>
+                                <TableCell>Notation</TableCell>
+                                <TableCell>Value</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Total Traffic, Variant</TableCell>
+                                <TableCell>n<sub>v</sub></TableCell>
+                                <TableCell>{formData.trafficVarInput}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Total Traffic, Control</TableCell>
+                                <TableCell>n<sub>c</sub></TableCell>
+                                <TableCell>{formData.trafficControlInput}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Number of Conversions, Variant</TableCell>
+                                <TableCell>y<sub>v</sub></TableCell>
+                                <TableCell>{formData.conVarInput}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Number of Conversions, Control</TableCell>
+                                <TableCell>y<sub>c</sub></TableCell>
+                                <TableCell>{formData.conControlInput}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Total Traffic, Control</TableCell>
+                                <TableCell>n<sub>v</sub></TableCell>
+                                <TableCell>{formData.trafficControlInput}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Estimate of Conversion Rate Variant</TableCell>
+                                <TableCell>p̂<sub>v</sub></TableCell>
+                                <TableCell>{formData.conVarInput / formData.trafficVarInput}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Estimate of Conversion Rate Control</TableCell>
+                                <TableCell>p̂<sub>c</sub></TableCell>
+                                <TableCell>{formData.conControlInput / formData.trafficControlInput}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Estimate of Pooled Conversion Rate</TableCell>
+                                <TableCell>p̂</TableCell>
+                                <TableCell>{pooledConversionRate.toPrecision(4)}</TableCell>
+                            </TableRow>
+                            <TableCell>Lift, Observed</TableCell>
+                            <TableCell><i>lift</i></TableCell>
+                            <TableCell>{liftEstimate.toPrecision(4)}</TableCell>
+                        </TableHead>
+                    </Table>
+                </TableContainer>
+            </Box>
+            <Box>
+                <div className="header">Validation of Testing Assumptions</div>
+                <div style={{ textAlign: "center" }}>The results of the "Post-Test Calculator" can only be used when the following three conditions are satisfied</div>
+                <ol>
+                    <li>
+                        Independence within groups
+                    </li>
+                    <ul><li>Answer intuitively. Could conversions be influenced by each other within the variant group? What about the control? </li></ul>
+                    <li>Independence between groups</li>
+                    <ul><li>Answer intuitively. Could conversions in the control group influence conversions in the variant group?</li></ul>
+                    <li>Normality</li>
+                    <ul><li>We will use a general rule of thumb, that we need at least 10 expected and observed conversions and 10 expected and observed non-conversions in each of the two groups (Calculated below)</li></ul>
+                </ol>
+            </Box>
+            <Box>
+                <TableContainer sx={{ height: "auto" }} component={Paper}>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Condition to Satisfy</TableCell>
+                                <TableCell>Equation to Satisfy</TableCell>
+                                <TableCell>Satisfied?</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableRow>
+                            <TableCell><i>Observed</i> number of conversions in variant group must be greater or equal to 10</TableCell>
+                            <TableCell>y<sub>v</sub> ≥ 10</TableCell>
+                            <TableCell>{(formData.conVarInput > 9) ? "True" : "False"}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell><i>Observed</i> number of conversions in control group must be greater or equal to 10</TableCell>
+                            <TableCell>y<sub>c</sub> ≥ 10</TableCell>
+                            <TableCell>{(formData.conControlInput > 9) ? "True" : "False"}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell><i>Observed</i> number of conversions in variant group must be greater or equal to 10</TableCell>
+                            <TableCell>n<sub>v</sub> - y<sub>v</sub> ≥ 10</TableCell>
+                            <TableCell>{(formData.trafficVarInput - formData.conVarInput > 9) ? "True" : "False"}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell><i>Observed</i> number of conversions in control group must be greater or equal to 10</TableCell>
+                            <TableCell>n<sub>c</sub> - y<sub>c</sub> ≥ 10</TableCell>
+                            <TableCell>{(formData.trafficControlInput - formData.conControlInput > 9) ? "True" : "False"}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell><i>Expected</i> number of conversions in variant group must be greater or equal to 10</TableCell>
+                            <TableCell>n<sub>v</sub>p̂ ≥ 10</TableCell>
+                            <TableCell>{(formData.trafficVarInput * pooledConversionRate > 9) ? "True" : "False"}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell><i>Observed</i> number of conversions in control group must be greater or equal to 10</TableCell>
+                            <TableCell>n<sub>c</sub>p̂ ≥ 10</TableCell>
+                            <TableCell>{(formData.trafficControlInput * pooledConversionRate > 9) ? "True" : "False"}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell><i>Observed</i> number of conversions in variant group must be greater or equal to 10</TableCell>
+                            <TableCell>n<sub>v</sub>{"(1 -p̂) ≥ 10"}</TableCell>
+                            <TableCell>{((formData.trafficVarInput * (1 - pooledConversionRate)) > 9) ? "True" : "False"}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell><i>Observed</i> number of conversions in control group must be greater or equal to 10</TableCell>
+                            <TableCell>n<sub>c</sub>{"(1 -p̂) ≥ 10"}</TableCell>
+                            <TableCell>{((formData.trafficControlInput * (1 - pooledConversionRate)) > 9) ? "True" : "False"}</TableCell>
+                        </TableRow>
+                    </Table>
+                </TableContainer>
+                <div style={{fontWeight:"bold"}}>Are all conditions satisfied? <span style={{fontWeight:"normal", backgroundColor: conditionSatisfied? "#6eb05d":"#b05d5d"} }>{conditionSatisfied? "True":"False"}</span></div>
+            </Box>
         </div>
     )
 }
