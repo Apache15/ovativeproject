f8b35539845e90163aa2524ee377ab96a0fe2aed
Apache15 <blake.engrav@gmail.com>
2023-04-10 22:57:45 -0500

    theme overhaul and binary posttest education

--- src/App.js
+++ src/App.js
@@ -1,17 +1,61 @@
 import './App.css';
-import {Button, Toolbar, AppBar, Box} from '@mui/material'
-import {useState, useEffect} from 'react';
+import { Button, Toolbar, AppBar, Box, ThemeProvider, createTheme, Table } from '@mui/material'
+import { useState, useEffect } from 'react';
 import ContinuousPreTestCalculator from './continuous-calculator/continuousPreTest';
 import BinomialPostTestCalculator from './binomial-posttest-calculator/binomial-posttest-calc';
 import BiPretest from './binomial-pretest/binomial-pretest-calc';
 import ContPostTest from './continuous-posttest/continuous-posttest-calc';
+
+
+const theme = createTheme({
+  components: {
+    MuiTooltip: {
+      styleOverrides: {
+        tooltip: {
+          fontSize: '1em'
+        }
+      }
+    },
+    MuiTable: {
+      styleOverrides: {
+        root: {
+          paddingBottom: "10px",
+          paddingTop: "10px",
+          border: "2px solid black",
+          borderRadius: "10px",
+        }
+      }
+    },
+    MuiTableCell: {
+      styleOverrides: {
+        root: {
+          borderBottom: "1px solid #686868",
+        }
+      }
+    },
+    MuiInputLabel:{
+      styleOverrides:{
+        root:{
+          fontSize:"22px",
+        }
+      }
+    },
+    MuiInput:{
+      styleOverrides:{
+        root:{
+          fontSize:"19px",
+        }
+      }
+    }
+  }
+})
 
 function App() {
   const pages = ['Continuous Pre Test', 'Continuous Post Test', 'Binomial Pre Test', 'Binomial Post Test'];
   const [activePage, changePage] = useState(0);
   const [pageData, changeData] = useState(0);
   useEffect(() => {
-    switch(activePage) {
+    switch (activePage) {
       case "Continuous Pre Test":
         changeData(<ContinuousPreTestCalculator />)
         break;
@@ -31,31 +75,33 @@ function App() {
 
   return (
     <div className="App">
-      <AppBar position='fixed'>
-        <Toolbar sx={{ backgroundColor: 'White'}}>
-          <Box sx={{flexGrow: 1, display: {xs: 'none', md: 'flex'} }}>
-            {pages.map((page) => (
-              <Button
-                onClick={()=>{changePage(page);}}
-                key={page}
-                sx={{
-                  my: 2, color: '#FA4616',
-                  display: 'block',
-                  fontFamily: "sans-serif",
-                  fontWeight: 700,
-                }}
-              > 
-                {page}
-              </Button>
-            ))}
-          </Box>
-        </Toolbar>
-      </AppBar>
-      <header className="App-header">
-        <div className="Calculator-window">
-          {pageData}
-        </div>
-      </header>
+      <ThemeProvider theme={theme}>
+        <AppBar position='fixed'>
+          <Toolbar sx={{ backgroundColor: 'White' }}>
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+              {pages.map((page) => (
+                <Button
+                  onClick={() => { changePage(page); }}
+                  key={page}
+                  sx={{
+                    my: 2, color: '#FA4616',
+                    display: 'block',
+                    fontFamily: "sans-serif",
+                    fontWeight: 700,
+                  }}
+                >
+                  {page}
+                </Button>
+              ))}
+            </Box>
+          </Toolbar>
+        </AppBar>
+        <header className="App-header">
+          <div className="Calculator-window">
+            {pageData}
+          </div>
+        </header>
+      </ThemeProvider>
     </div>
   );
 }
